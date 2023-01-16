53b64bccb361c6700745425be3bb322983ab8c15
Jardel Mandel Cardoso <jardel.mandel@gmail.com>
2023-01-16 13:43:09 -0300

    Add project files.

---
+++ Ciclos/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ciclos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Ciclo While
+            //int valor = 0;
+
+            //while (valor < 10)
+            //{
+            //    Console.WriteLine("O valor é " + valor);
+            //    valor++;
+            //}
+            //Console.ReadKey();
+            #endregion
+
+            #region Ciclo do while
+            //int valor = 15;
+            //do
+            //{
+            //    Console.WriteLine("O valor é " + valor);
+            //    valor++;
+            //} while (valor < 10);
+            #endregion
+
+            #region Ciclo for
+
+            //for (int i = 0, j = 9; i < 10; i++, j--)
+            //{
+            //    Console.WriteLine("O valor de I é " + i + " e de J é " + j);
+            //}
+
+            #endregion
+
+            #region Ciclo for each
+
+            //string[] nomes = { "Jardel", "Mandel", "Cardoso", "Camila" };
+
+            //foreach (string nome in nomes)
+            //{
+            //    Console.WriteLine("Nome: " + nome);
+            //}
+
+            #endregion
+
+            Console.ReadKey();
+        }
+    }
+}
